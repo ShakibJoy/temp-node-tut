4349a980babab427caba2ab116bd57f2c8c41d5a
ShakibJoy <shakibtaher69@gmail.com>
2024-01-04 18:21:02 +0600

    first commit

---
+++ 3-module.js
@@ -0,0 +1,7 @@
+const names = require('./4-module')
+const sayHi = require('./5-module')
+const data = require('./6-alternative-flavour')
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
